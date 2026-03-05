2645152f78f73bfaa338df38a5b71bbb0b7531a7
Pedroh256 <pedroh93601@gmail.com>
2026-03-04 22:51:28 -0400

    fix: guard pages authenticated

--- frontend/app/(authenticated)/layout.tsx
+++ frontend/app/(authenticated)/layout.tsx
@@ -1,24 +1,25 @@
 import React from "react";
 import { AuthProvider } from "./context/AuthContext";
 import Layout from "./_components/Layout";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
 
 export default async function AuthenticatedLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  //   const e = await new Promise<boolean>((resolve,reject) => {
-  //     setTimeout(() => {
-  //         resolve(true)
-  //     },100);
-  //   });
+  const session = await getServerSession();
+  if(!session){
+    return redirect("/unauthorized")
+  }
   return (
     <main>
       <AuthProvider>
         <Layout>
-        {children}
+          {children}
         </Layout>
-        </AuthProvider>
+      </AuthProvider>
     </main>
   );
 }
